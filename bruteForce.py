2adff239c0440ff0ab4b5190ba2492feb7b2051f
Amir Sepehri <amir.spr76@gmail.com>
2017-05-18 14:14:33 +0430

    fixed and working for number array

--- bruteForce.py
+++ bruteForce.py
@@ -1,23 +1,29 @@
 array = [1,2,1,2,4,2,3,5,2,1,6,2,4,8,2,1]
+def areMotif(arr1,arr2,k,tolerance):
+    i=0
+    j=0
+    counter = 0
+    while (i,j < k-1):
+        if(arr1[i]==arr2[j]):
+            counter +=1
+        elif(arr1[i]!=arr2[j]):
+            tolerance-=1
+            counter+=1
+        if(tolerance<0):
+            return False
+        elif (counter==k) :
+            return True
+        i+=1
+        j+=1
 
 def bruteForce(array,k,tolerance):
     for i in range(len(array)):
-        temp = array[i+1:i+k+1]
+        temp = array[i:i+k]
         if (len(temp)==k):
             for j in range(i+1,len(array)):
-                counter = 0
-                tempTol = tolerance
-                valid = 1
-                for z in range(k):
-                    if (temp[z]==array[j]):
-                        counter+=1
-                    elif (temp[z]!=array[j]):
-                        tempTol -=1
-                    if (tempTol<0):
-                        valid =0
-                        break
-                if (valid==1):
-                    print(array[i:i+k])
+                if(j+k<=len(array)-1 and areMotif(temp,array[j:j+k],k,tolerance)==True):
                     print(temp[:])
+                    print(array[j:j+k])
+
 
 bruteForce(array,3,1)
