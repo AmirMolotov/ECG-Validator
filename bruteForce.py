e276ab8f10b8aba5c225e8e4dcb33cddaa56d819
Amir Sepehri <amir.spr76@gmail.com>
2017-05-20 22:24:08 +0430

    Now it works non overlappingly

--- bruteForce.py
+++ bruteForce.py
@@ -1,9 +1,9 @@
-array = [1,2,4,1,2,4,1,2,5]
+array ="ABCABDABCAFkkkkkkkkC" 
 def areMotif(arr1,arr2,k,tolerance):
     i=0
     j=0
     counter = 0
-    while (i,j < k-1):
+    while (i,j < k):
         if(arr1[i]==arr2[j]):
             counter +=1
         elif(arr1[i]!=arr2[j]):
@@ -17,13 +17,18 @@ def areMotif(arr1,arr2,k,tolerance):
         j+=1
 
 def bruteForce(array,k,tolerance):
+    flag = 0
     for i in range(len(array)):
         temp = array[i:i+k]
         if (len(temp)==k):
-            for j in range(i+1,len(array)):
+            for j in range(i+k,len(array)):
                 if(j+k<=len(array) and areMotif(temp,array[j:j+k],k,tolerance)==True):
-                    print(temp[:])
+                    if (flag != 1):
+                        print(temp[:])
+                    flag = 1
                     print(array[j:j+k])
+            print
+            flag = 0
 
 
 bruteForce(array,3,1)
